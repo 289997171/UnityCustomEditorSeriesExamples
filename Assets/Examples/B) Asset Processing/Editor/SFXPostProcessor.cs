b3e052d3b08b74df7d42e2a8794c5098080c9765
289997171 <289997171@qq.com>
2016-06-22 14:11:56 +0800

    AssetPostprocessor 资源处理器

--- Assets/Examples/B) Asset Processing/Editor/SFXPostProcessor.cs
+++ Assets/Examples/B) Asset Processing/Editor/SFXPostProcessor.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEditor;
 using UnityEngine;
-using System.Collections;
 
+
+/// <summary>
+/// 资源处理器
+/// 让你钩进导入管线，在运行脚本之前或导入资源之后。
+/// 这样你可以在导入的设置中重载默认值或修改导入的数据，如纹理，网格，音效。
+/// </summary>
 public class SFXPostProcessor : AssetPostprocessor {
 
+
+    /// <summary>
+    /// 处理音效
+    /// </summary>
     void OnPreprocessAudio()
     {
         AssetImporter importer = assetImporter;
